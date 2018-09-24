54389441797933b555549c7f4844f02ed2442f07
Android Studio <android@senacinformatica.br>
2018-09-24 11:23:17 -0300

    Criaçao do projeto

---
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/CadastroActivity.java
@@ -0,0 +1,15 @@
+package com.inf3m171.ltem.appclinicaltem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class CadastroActivity extends AppCompatActivity {
+    private EditText etEmail
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
