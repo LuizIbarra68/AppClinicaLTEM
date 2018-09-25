dac9de8d984e2f6e1e9a44972c950b8281d6c455
Android Studio <android@senacinformatica.br>
2018-09-25 10:43:53 -0300

    Criacao tela marcacao em andamento

---
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/MenuUsuarioActivity.java
@@ -0,0 +1,13 @@
+package com.inf3m171.ltem.appclinicaltem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.MenuUsuario);
+    }
+}
