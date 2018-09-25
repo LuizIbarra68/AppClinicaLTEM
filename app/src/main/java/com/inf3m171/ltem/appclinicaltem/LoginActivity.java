367df9b6504d5992c8ba71c59f407b0b932ea3e7
Android Studio <android@senacinformatica.br>
2018-09-25 11:36:22 -0300

    Troca de button para textview no cadastrar do login

--- app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
@@ -1,22 +1,36 @@
 package com.inf3m171.ltem.appclinicaltem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText etlogin, etSenha;
-    private Button btnEntrar, btnCadastrar;
+    private EditText etEmail, etSenha;
+    private Button btnEntrar;
+    TextView tvCadastrar;
 
-
+//    private FirebaseAuth autenticacao;
+//    private FirebaseAuth
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        etlogin = (EditText) findViewById(R.id.et)
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etSenha = (EditText) findViewById(R.id.etSenha);
+
+        btnEntrar = (Button) findViewById(R.id.btnEntrar);
+        tvCadastrar = (TextView) findViewById(R.id.tvCadastrar);
+
+
+
+//        btnCadastrar.setOnClickListener() {
+//            Intent i = new Intent(LoginActivity.this), Cadastr
+
     }
 }
