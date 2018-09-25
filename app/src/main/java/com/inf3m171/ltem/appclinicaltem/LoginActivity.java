4dd54d5090300a3d093e2739d272a5f1a4879e98
Android Studio <android@senacinformatica.br>
2018-09-25 12:12:05 -0300

    Iniado codigo tela login activity

--- app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.inf3m171.ltem.appclinicaltem;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,8 +18,8 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnEntrar;
     TextView tvCadastrar;
 
-//    private FirebaseAuth autenticacao;
-//    private FirebaseAuth
+    private FirebaseAuth autenticacao;
+    private FirebaseAuth.AuthStateListener stateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +33,40 @@ public class LoginActivity extends AppCompatActivity {
         tvCadastrar = (TextView) findViewById(R.id.tvCadastrar);
 
 
+        autenticacao = FirebaseAuth.getInstance();
 
-//        btnCadastrar.setOnClickListener() {
-//            Intent i = new Intent(LoginActivity.this), Cadastr
+        tvCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this,
+                        CadastroActivity.class);
+                startActivity(i);
+            }
+        });
+        stateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Intent i = new Intent(LoginActivity.this,
+                            MenuUsuarioActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                entrar();
+            }
+
+        });
 
     }
+
+    private void entrar(){
+
+    }
+
 }
