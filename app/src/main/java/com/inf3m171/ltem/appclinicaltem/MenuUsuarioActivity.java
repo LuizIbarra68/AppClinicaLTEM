25495e8feaf110576ae045a6c23d5aaf2c6d7c6f
Android Studio <android@senacinformatica.br>
2018-09-25 11:20:05 -0300

    Atualizaçao menu usuario

--- app/src/main/java/com/inf3m171/ltem/appclinicaltem/MenuUsuarioActivity.java
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/MenuUsuarioActivity.java
@@ -8,6 +8,6 @@ public class MenuUsuarioActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menuusuario);
+        setContentView(R.layout.menu_usuario);
     }
 }
