dede3a92c3fa70e2e128721037c6f8f7a21f3f92
Android Studio <android@senacinformatica.br>
2018-09-25 11:16:25 -0300

    Atualizaçao nome tela de menu

--- app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
@@ -2,12 +2,21 @@ package com.inf3m171.ltem.appclinicaltem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etlogin, etSenha;
+    private Button btnEntrar, btnCadastrar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etlogin = (EditText) findViewById(R.id.et)
     }
 }
