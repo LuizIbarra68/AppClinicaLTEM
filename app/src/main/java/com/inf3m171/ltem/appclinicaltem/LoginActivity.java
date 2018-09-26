5f101f77caca7f04c93042d238ec6f1ed75022be
Android Studio <android@senacinformatica.br>
2018-09-26 10:11:41 -0300

    Tela login programada

--- app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
@@ -8,7 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -65,7 +69,37 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        autenticacao.addAuthStateListener(stateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if ( stateListener != null){
+            autenticacao.removeAuthStateListener(stateListener);
+        }
+    }
+
     private void entrar(){
+        String email = etEmail.getText().toString();
+        String senha = etSenha.getText().toString();
+
+        if ( !email.isEmpty()){
+            autenticacao.signInWithEmailAndPassword(email, senha)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if ( !task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this,
+                                        "Usuário ou senha inválidos", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+        }
 
     }
 
