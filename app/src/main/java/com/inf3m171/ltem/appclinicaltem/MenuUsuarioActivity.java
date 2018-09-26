e2c2757389b9952906b5bc3b17e90b496baed639
Android Studio <android@senacinformatica.br>
2018-09-26 12:18:01 -0300

    programada atela cadastro usuario

--- app/src/main/java/com/inf3m171/ltem/appclinicaltem/MenuUsuarioActivity.java
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/MenuUsuarioActivity.java
@@ -1,13 +1,30 @@
 package com.inf3m171.ltem.appclinicaltem;
 
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MenuUsuarioActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_usuario);
+
+
     }
-}
+
+    }
+
