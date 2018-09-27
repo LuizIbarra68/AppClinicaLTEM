2e093bd33198949e37209bca739d348525203015
tvvieira <tamiresvvieira1@hotmail.com>
2018-09-27 09:53:53 -0300

    botão sair adicionado

--- app/src/main/java/com/inf3m171/ltem/appclinicaltem/MenuUsuarioActivity.java
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/MenuUsuarioActivity.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +26,25 @@ public class MenuUsuarioActivity extends AppCompatActivity {
         setContentView(R.layout.menu_usuario);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        menu.add("Sair");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.toString().equals("Sair")){
+            FirebaseAuth.getInstance().signOut();
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     }
