e0a84174bb0e5ea69203731d5dea746b4b1a2516
Lucas Cruz <el.souza2010@hotmail.com>
2018-10-01 10:09:57 -0300

    land /en

--- app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
@@ -1,27 +1,15 @@
 package com.inf3m171.ltem.appclinicaltem;
-
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.provider.Contacts;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -29,8 +17,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.inf3m171.ltem.appclinicaltem.model.Consulta;
 
 public class LoginActivity extends AppCompatActivity {
 
