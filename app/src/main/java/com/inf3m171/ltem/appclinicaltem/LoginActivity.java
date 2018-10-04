41418c32926590d62041eea7e40d4f4ec89890a3
Tamires Vieira <tamiresvvieira1@hotmail.com>
2018-10-04 09:30:01 -0300

    btn remarcar funcionando

--- app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
+++ app/src/main/java/com/inf3m171/ltem/appclinicaltem/LoginActivity.java
@@ -13,15 +13,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -29,7 +20,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.inf3m171.ltem.appclinicaltem.model.Consulta;
 
 public class LoginActivity extends AppCompatActivity {
